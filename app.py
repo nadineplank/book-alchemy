c423f7feeb2147bda39fdedb23d113c15a456f37
Nadine <nadine@contentpass.de>
2026-09-15 13:43:29 +0200

    Add 'add book' route

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 db.init_app(app)
 
 
+def get_authors():
+    """Return a list of all authors in the database."""
+    authors = Author.query.all()
+    return authors
+
 @app.route('/add_author', methods=['GET', 'POST'])
 def add_author():
     """Show the add-author form and handle new author submissions."""
@@ -30,6 +35,28 @@ def add_author():
         return render_template('add_author.html', message='Author added successfully!')
     return render_template('add_author.html')
 
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    """Show the add-book form and handle new book submissions."""
+    current_year = date.today().year
+    authors = get_authors()
+
+    if request.method == 'POST':
+
+        book = Book(
+            title=request.form.get('title'),
+            isbn=request.form.get('isbn'),
+            publication_year=request.form.get('publication_year', type=int),
+            author_id=request.form.get('author_id', type=int)
+        )
+
+        db.session.add(book)
+        db.session.commit()
+
+        return render_template('add_book.html', message='Book added successfully!', current_year=current_year, authors=authors)
+
+    return render_template('add_book.html', current_year=current_year, authors=authors)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
