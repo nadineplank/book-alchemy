70915a870b2a79c89f1daab15a3ca6c2562b1ee8
Nadine <nadine@contentpass.de>
2026-09-15 12:32:07 +0200

    Add templates

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import os
+
+from data_models import db, Author, Book
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+
+db.init_app(app)
+
+# with app.app_context():
+#   db.create_all()
